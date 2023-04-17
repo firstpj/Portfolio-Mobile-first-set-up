f3df59e8a04017398b2fcd1524a76ebc01730680
firstpj <114886262+firstpj@users.noreply.github.com>
2023-04-17 14:50:11 +0100

    Fix linter errors

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const menu = document.getElementById('menu-');
-    const button = document.getElementById('mob-barss');
-    const closebtn = document.getElementById('closed');
-    const menuitems = document.getElementById('mlist');
-  
-    button.onclick = function showMenu() {
-      menu.style.display = 'block';
-    };
-  
-    closebtn.onclick = function hideMenu() {
-      menu.style.display = 'none';
-    };
-  
-    menuitems.onclick = function hideMenu() {
-      menu.style.display = 'none';
-    };
-  });
-  
+  const menu = document.getElementById('menu-');
+  const button = document.getElementById('mob-barss');
+  const closebtn = document.getElementById('closed');
+  const menuitems = document.getElementById('mlist');
+
+  button.onclick = function showMenu() {
+    menu.style.display = 'block';
+  };
+
+  closebtn.onclick = function hideMenu() {
+    menu.style.display = 'none';
+  };
+
+  menuitems.onclick = function hideMenu() {
+    menu.style.display = 'none';
+  };
+});
