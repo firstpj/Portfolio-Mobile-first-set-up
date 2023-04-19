907588a673ce552566910364c4ce40b45154655c
firstpj <114886262+firstpj@users.noreply.github.com>
2023-04-19 15:27:41 +0100

    modified index.html index.js and css files

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const menu = document.getElementById('menu-');
-  const button = document.getElementById('mob-barss');
-  const closebtn = document.getElementById('closed');
-  const menuitems = document.getElementById('mlist');
-
-  button.onclick = function showMenu() {
-    menu.style.display = 'block';
-  };
-
-  closebtn.onclick = function hideMenu() {
-    menu.style.display = 'none';
-  };
-
-  menuitems.onclick = function hideMenu() {
-    menu.style.display = 'none';
-  };
+const navOne = document.getElementById('nav-one');
+const navTwo = document.getElementById('nav-two');
+const navThree = document.getElementById('nav-three');
+const nav = document.getElementById('nav');
+const btn = document.getElementById('btn');
+navOne.addEventListener('click', () => {
+  nav.classList.add('test-nav');
+});
+navTwo.addEventListener('click', () => {
+  nav.classList.add('test-nav');
+});
+navThree.addEventListener('click', () => {
+  nav.classList.add('test-nav');
+});
+btn.addEventListener('click', () => {
+  nav.classList.remove('test-nav');
 });
