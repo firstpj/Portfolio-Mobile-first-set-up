24a1c7598ff89313f9a8b3272bdf24d968c1f8fc
firstpj <114886262+firstpj@users.noreply.github.com>
2023-04-20 18:16:35 +0100

    index.js file

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
 
 function toggleMenu() {
-  if (menu.classList.contains('showMenu')) {
+  if(menu.classList.contains('showMenu')) {
 		menu.classList.remove('showMenu');
-	   closeIcon.style.display = 'none';
-	   menuIcon.style.display = 'block';
-   } else {
+	  closeIcon.style.display = 'none';
+	  menuIcon.style.display = 'block';
+		} else {
   	menu.classList.add('showMenu');
 	  closeIcon.setAttribute('style', 'display: block !important');
 	  menuIcon.style.display = 'none';
