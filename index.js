619ff4b683a88cfc0008065aa75d4edfab589abb
firstpj <114886262+firstpj@users.noreply.github.com>
2023-04-20 18:30:35 +0100

    index.js file

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const closeIcon = document.querySelector('.closeIcon');
 const menuIcon = document.querySelector('.menuIcon');
 
 function toggleMenu() {
-  if(menu.classList.contains('showMenu')) {
-	  menu.classList.remove('showMenu');
-	 closeIcon.style.display = 'none';
-	 menuIcon.style.display = 'block';
+  if (menu.classList.contains('showMenu')) {
+		menu.classList.remove('showMenu');
+	  closeIcon.style.display = 'none';
+	  menuIcon.style.display = 'block';
 		} else {
   	menu.classList.add('showMenu');
 	  closeIcon.setAttribute('style', 'display: block !important');
